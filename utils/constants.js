55d6fd803cb4771b796e1bb777ec2019bbf7b463
Francesco Barbieri <checcobarbieri@gmail.com>
2022-02-24 20:02:23 +0100

    remove db config

--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,2 @@
-//removed for security reason
+exports.JWT_SECRET = 'secrettreedomtest';
+exports.MONGODB_URI = 'mongodb+srv://checco:CHEcol_diurywq29@cluster0.lddrl.mongodb.net/treedomtest_restapi?retryWrites=true&w=majority';
