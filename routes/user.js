18caf19f6893c662aa24fb39ebf4edab66ce9075
Francesco Barbieri <checcobarbieri@gmail.com>
2022-03-14 14:19:32 +0100

    docker init

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ const userController = require('../controllers/user');
 
 const router = express.Router();
 
+//router.get('/users', authorize('admin'), userController.getUsers);
+
 router.get('/name', authorize(), userController.getName);
 
 router.put('/name', authorize(), userController.updateName);
--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,2 @@
 exports.JWT_SECRET = 'secrettreedomtest';
-exports.MONGODB_URI = '';
+exports.MONGODB_URI = 'mongodb://root:rootpassword@localhost:27017/treedomtest?authSource=admin';
